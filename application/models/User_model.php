6b8ffbdd3950de9d409ca667b6d0fefe3ac41868
tulsiwartiz <84794971+tulsiwartiz@users.noreply.github.com>
2022-09-14 13:19:11 +0530

    Update User_model.php

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -4,7 +4,7 @@ class User_model extends CI_Model
 {
 	public function __construct() {
         parent::__construct();
-		// $this->load->library('session');
+		
         $this->load->database();
     }
 	
