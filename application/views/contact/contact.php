8762d74cca0fc541b97680b63a6b1761216bf371
tulsiwartiz <84794971+tulsiwartiz@users.noreply.github.com>
2022-10-31 15:52:52 +0530

    Update contact.php (#32)

--- application/views/contact/contact.php
+++ application/views/contact/contact.php
@@ -16,7 +16,6 @@ $Province  = $this->config->item('Province');
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
         <link href="<?php echo base_url()?>assets/css/styles.css" rel="stylesheet" />
 		<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="<?php echo base_url() ?>assets/js/plugins/magnific-popup/magnific-popup.css">
