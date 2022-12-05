fb9cb87aaed62385b238bc8ce72456713e481f13
tulsiwartiz <84794971+tulsiwartiz@users.noreply.github.com>
2022-12-05 15:48:49 +0530

    Update MY_Controller.php

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -3,10 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class MY_Controller extends CI_Controller {
 
-	/**
-		Main Controller file
-		Common function here 
-	**/
 	public function __construct() {
 
 		parent::__construct();
